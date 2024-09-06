93c71f078ee69233a553cdf46a03460df1031efe
Gustav Lindberg <gustav@gustavlindberg.se>
2024-09-06 23:23:55 +0200

    Use clvv version

--- Formula/fasd-guusli.rb
+++ Formula/fasd-guusli.rb
@@ -1,13 +1,13 @@
 class FasdGuusli < Formula
   desc "CLI tool for quick access to files and directories"
-  homepage "https://github.com/guusli/fasd"
-  url "https://github.com/guusli/fasd/archive/refs/tags/1.0.1.tar.gz"
+  homepage "https://github.com/clvv/fasd"
+  url "https://github.com/clvv/fasd/archive/refs/tags/1.0.1.tar.gz"
   sha256 "88efdfbbed8df408699a14fa6c567450bf86480f5ff3dde42d0b3e1dee731f65"
   license "MIT"
-  head "https://github.com/guusli/fasd.git", branch: "master"
+  head "https://github.com/clvv/fasd.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ef3e9e128885144c04e3c09b5604330dfd076ad68df428a80026fd0164fee5e0"
+    sha256 cellar: :any_skip_relocation, all: "9241df0f32971ce5a84c977f6908b93114946843813d5375ba7b983a7a783188"
   end
 
   def install
@@ -19,4 +19,3 @@ class FasdGuusli < Formula
     system "#{bin}/fasd", "--init", "auto"
   end
 end
-
